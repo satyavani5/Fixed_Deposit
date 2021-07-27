1552967ddb3f74a523f5a9edca812464a96153db
satyavani <psatyavani891@gmail.com>
2021-07-27 15:45:47 +0530

    Fixed_Deposit code developed

---
+++ Fixed_Deposit/src/main/java/ICICI/IndexController.java
@@ -0,0 +1,34 @@
+package ICICI;
+
+import java.util.Date;
+
+import com.khubla.pragmatach.framework.annotation.Controller;
+import com.khubla.pragmatach.framework.annotation.Route;
+import com.khubla.pragmatach.framework.annotation.View;
+import com.khubla.pragmatach.framework.api.PragmatachException;
+import com.khubla.pragmatach.framework.api.Response;
+import com.khubla.pragmatach.plugin.freemarker.FreemarkerController;
+
+@Controller(name="IndexController")
+@View(view="index.ftl")
+public class IndexController extends FreemarkerController {
+   
+   /**
+    * the message
+    */
+   private final String message = "Welcome to Pragmatach";
+
+
+   public String getMessage() {
+      return message;
+   }
+
+   @Route(uri = "/")
+   public Response render() throws PragmatachException {
+      return super.render();
+   }
+
+   public String getTime() {
+      return new Date().toString();
+   }
+}
